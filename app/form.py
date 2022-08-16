ab7684333bc0ed6eabd8320e0a6fb02d44518e7d
saitejareddy <tejareddy6933@gmail.com>
2022-08-16 20:58:37 +1000

    feat (removed number line)

--- app/form.py
+++ app/form.py
@@ -5,5 +5,4 @@ from wtforms import SubmitField, IntegerField
 class AddNumbersForm(FlaskForm):
     num1 = IntegerField('What is number 1?')
     num2 = IntegerField('What is number 2?')
-    num3 = IntegerField('what is number 3?')
     submit = SubmitField('Add!')
