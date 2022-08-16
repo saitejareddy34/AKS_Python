49276493c86a5d6afad13f949f1a13e154169069
saitejareddy <tejareddy6933@gmail.com>
2022-08-16 20:52:34 +1000

    feat(added third number)

--- app/form.py
+++ app/form.py
@@ -5,4 +5,5 @@ from wtforms import SubmitField, IntegerField
 class AddNumbersForm(FlaskForm):
     num1 = IntegerField('What is number 1?')
     num2 = IntegerField('What is number 2?')
+    num3 = IntegerField('what is number 3?')
     submit = SubmitField('Add!')
